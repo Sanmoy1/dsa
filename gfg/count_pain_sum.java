4e6f64f62f44d86a69edef6e277fa424062e4ed2
Sanmoy1 <emailofsanmoyroy@gmail.com>
2025-05-17 02:00:54 +0530

    valid sudoku

--- gfg/count_pain_sum.java
+++ gfg/count_pain_sum.java
@@ -1,6 +1,7 @@
 //https://www.geeksforgeeks.org/problems/count-pair-sum5956/1?page=1&category=Hash&sortBy=difficulty
 
 //here we are using two pointer approach just like binary search  as the array is sorted
+//O(n+m) 
 
 public class Solution {
     static int countPairs(int arr1[], int arr2[], int x) {
@@ -27,4 +28,32 @@ public class Solution {
         }
         return count;
     }
+}
+
+//solve using hash map
+// generally we use hashmap when the array is not sorted but we can also achieve O(n+m) time complexity
+
+static int countPairs(int arr1[], int arr2[], int x) {
+    // Initialize variables
+    int count = 0;
+    int n = arr1.length;
+    int m = arr2.length;
+    
+    // Create a hashmap to store elements of arr2
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    // Populate hashmap with elements from arr2
+    for (int i = 0; i < m; i++) {
+        map.put(arr2[i], map.getOrDefault(arr2[i], 0) + 1);
+    }
+    
+    // Iterate through arr1 to find pairs
+    for (int i = 0; i < n; i++) {
+        int target = x - arr1[i];
+        if (map.containsKey(target)) {
+            count += map.get(target); // Add the frequency of target
+        }
+    }
+    
+    return count;
 }
