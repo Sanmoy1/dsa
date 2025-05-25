773aecc75b72b6c6672fc6edd31428e5c981b247
Sanmoy1 <emailofsanmoyroy@gmail.com>
2025-05-26 01:55:24 +0530

    new commit

--- leetcode-practise/zero_array_transformation.java
+++ leetcode-practise/zero_array_transformation.java
@@ -29,5 +29,32 @@ public class zero_array_transformation {
         return true;
 
     }
+
+    //complexity O(m+n)
+    // here we are using prefix sum (difference array)
+
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+            diff[l]++;
+            if (r + 1 < n) {
+                diff[r + 1]--;
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += diff[i];
+            if (nums[i] > cnt) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     
 }
