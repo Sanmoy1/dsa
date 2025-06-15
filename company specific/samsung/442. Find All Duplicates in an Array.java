3fa952bf3d2dfe9e5aad58cbe1b9b7b250a978d2
Sanmoy1 <emailofsanmoyroy@gmail.com>
2025-06-15 15:53:39 +0530

    add optimized solution to find duplicates in an array using O(1) space

--- company specific/samsung/442. Find All Duplicates in an Array.java
+++ company specific/samsung/442. Find All Duplicates in an Array.java
@@ -20,4 +20,19 @@ class Solution {
         }
         return arr;
     }
+    // here we have to change the value of the element in the array to negative if we have visited the number already
+    // time complexity O(n)
+    //space complexity O(1)
+    public List<Integer> findDuplicates(int[] nums) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int position=nums[Math.abs(nums[i])-1];
+            if(position<0)
+            ans.add(Math.abs(nums[i]));
+            else
+            nums[Math.abs(nums[i])-1]=position-2*position;
+        }
+        return ans;
+    }
 }
